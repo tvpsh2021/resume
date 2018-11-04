bed7e7b2e0afbb245ac490e74ba69e4c869dd48d
Jimmy Cheng <tvpsh2020@gmail.com>
2018-11-05 00:16:41 +0800

    Modify apps

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ const helmet = require('helmet');
 const app = express();
 const isDevelopmentEnvironment = process.env.NODE_ENV === 'development' ? true : false;
 
-// if (!isDevelopmentEnvironment) {
-// 	app.disable('x-powered-by');
-// 	app.enable('trust proxy');
-// 	app.use(helmet.hsts({
-// 		maxAge: 31536000,
-// 		includeSubDomains: true,
-// 		preload: true,
-// 		setIf: function (req) {
-// 			return req.secure;
-// 		}
-// 	}));
-// 	app.use((req, res, next) => {
-// 		if (!isDevelopmentEnvironment && !req.secure) {
-// 			res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
-// 		}
-// 		next();
-// 	});
-// }
+if (!isDevelopmentEnvironment) {
+	app.disable('x-powered-by');
+	app.enable('trust proxy');
+	app.use(helmet.hsts({
+		maxAge: 31536000,
+		includeSubDomains: false,
+		preload: false,
+		setIf: (req, res) => {
+			return req.secure || (req.headers['x-forwarded-proto'] === 'https');
+		}
+	}));
+	app.use((req, res, next) => {
+		if (!isDevelopmentEnvironment && !req.secure) {
+			// console.log('req.headers.host -> ', req.headers.host);
+			// console.log('req.headers.host -> ', req.originalUrl.originalUrl);
+			res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
+		}
+		next();
+	});
+}
 
 app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public'));
@@ -28,10 +30,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
+	// console.log('req.headers.host -> ', req.headers.host);
+	// console.log('req.headers.host -> ', req.originalUrl);
 	res.render('pages/resume.pug');
 });
 
 app.get('/portfolio', (req, res) => {
+	// console.log('req.headers.host -> ', req.headers.host);
+	// console.log('req.headers.host -> ', req.originalUrl);
 	res.render('pages/portfolio.pug');
 });
 
