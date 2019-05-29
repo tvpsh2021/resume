d1f57379c048ba1e7659d02c55f3fda3c46b48b0
Jimmy Cheng <tvpsh2020@gmail.com>
2019-05-29 14:18:28 +0800

    Update job description for recently one

--- public/js/script.js
+++ public/js/script.js
@@ -1,38 +1,38 @@
 (function (w, d, s, l, i) {
-	w[l] = w[l] || [];
-	w[l].push({
-		'gtm.start': new Date().getTime(),
-		event:'gtm.js'
-	});
-	var f = d.getElementsByTagName(s)[0],
-		j = d.createElement(s),
-		dl = l != 'dataLayer' ? '&l='+l : '';
+  w[l] = w[l] || [];
+  w[l].push({
+    'gtm.start': new Date().getTime(),
+    event:'gtm.js'
+  });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != 'dataLayer' ? '&l='+l : '';
 
-	j.async = true;
-	j.src='//www.googletagmanager.com/gtm.js?id=' + i + dl;
-	f.parentNode.insertBefore(j, f);
+  j.async = true;
+  j.src='//www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', 'GTM-N5NTRF');
 
 $(document).ready(function () {
-	if ($('#back-to-top').length) {
-		var scrollTrigger = 100, // px
-			backToTop = function () {
-				var scrollTop = $(window).scrollTop();
-				if (scrollTop > scrollTrigger) {
-					$('#back-to-top').addClass('show');
-				} else {
-					$('#back-to-top').removeClass('show');
-				}
-			};
-		backToTop();
-		$(window).on('scroll', function () {
-			backToTop();
-		});
-		$('#back-to-top').on('click', function (e) {
-			e.preventDefault();
-			$('html,body').animate({
-				scrollTop: 0
-			}, 700);
-		});
-	}
+  if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+      backToTop = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+          $('#back-to-top').addClass('show');
+        } else {
+          $('#back-to-top').removeClass('show');
+        }
+      };
+    backToTop();
+    $(window).on('scroll', function () {
+      backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate({
+        scrollTop: 0
+      }, 700);
+    });
+  }
 });
