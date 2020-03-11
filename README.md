f70db3d0bc5628fae9d944e883982bdfb96ac840
Jimmy Cheng <tvpsh2021@gmail.com>
2020-03-12 02:13:55 +0800

    Update some part of resume

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Jimmmmy Resume
+# Resume
 
-This is my resume, host on Google App Engine, written in [Pug](https://pugjs.org/api/getting-started.html), you can print it to paper or PDF file directly because it is optimized for printing, click [here](https://jimmmmy.com) to take a look!
+This is my resume, host on Google App Engine, written in [Pug](https://pugjs.org/api/getting-started.html) and Node.js, you can print it out or save as PDF file directly, it's optimized for printing, click [here](https://jimmmmy.com) to take a look!
+
+## CI flow
+
+- Commit to repository origin with `master` branch, this will trigger Google Cloud Build and deploy to Google App Engine with default service
 
 ## Development
 
--- data/sidebar.js
+++ data/sidebar.js
@@ -1,86 +1,86 @@
 module.exports = {
-	avatar: {
-		description: 'Web Developer',
-		name: 'JIMMY CHENG'
-	},
-	personal: {
-		fullName: {
-			title: 'Full Name',
-			content: '鄭景仁 / Chin-Jen Cheng',
-		},
-		birthday: {
-			title: 'Birthday',
-			content: 'January 6, 1990',
-		},
-		nationality: {
-			title: 'Nationality',
-			content: 'Taiwan',
-		},
-		languages: {
-			title: 'Languages',
-			content: '中文, English',
-		},
-	},
-	contact: {
-		address1: {
-			title: 'Address',
-			content: '3F, No.378, Nanxiang Rd.,',
-		},
-		address2: {
-			title: '',
-			content: 'Luzhu District,',
-			noPrint: true,
-		},
-		address3: {
-			title: '',
-			content: 'Taoyuan City, Taiwan, 33854',
-			noPrint: true,
-		},
-		phone: {
-			title: 'Phone',
-			content: '+886 931 077 193',
-		},
-		email: {
-			title: 'Email',
-			content: 'tvpsh2020@gmail.com',
-			link: 'mailto:tvpsh2020@gmail.com',
-		},
-		portfolio: {
-			title: 'Portfolio',
-			content: 'jimmmmy.com/portfolio',
-			link: 'https://jimmmmy.com/portfolio',
-		},
-	},
-	links: {
-		github: {
-			title: 'GitHub',
-			content: 'github.com/tvpsh2020',
-			link: 'https://github.com/tvpsh2020/',
-			icon: 'fa fa-github fa-2x',
-		},
-		linkedIn: {
-			title: 'LinkedIn',
-			content: 'linkedin.com/in/tvpsh2020',
-			link: 'https://www.linkedin.com/in/tvpsh2020/',
-			icon: 'fa fa-linkedin fa-2x',
-		},
-		blog: {
-			title: 'Blog',
-			content: 'blog.jimmmmy.com',
-			link: 'https://blog.jimmmmy.com/',
-			icon: 'fa fa-wordpress fa-2x',
-		},
-		facebook: {
-			title: 'Facebook',
-			content: 'jimmmmy.cheng',
-			link: 'https://www.facebook.com/jimmmmy.cheng/',
-			icon: 'fa fa-facebook fa-2x',
-		},
-		instagram: {
-			title: 'Instagram',
-			content: '@tvpsh2020',
-			link: 'https://www.instagram.com/tvpsh2020/',
-			icon: 'fa fa-instagram fa-2x',
-		},
-	}
+  avatar: {
+    description: 'Web Developer',
+    name: 'JIMMY CHENG'
+  },
+  personal: {
+    fullName: {
+      title: 'Full Name',
+      content: '鄭景仁 / Chin-Jen Cheng',
+    },
+    birthday: {
+      title: 'Birthday',
+      content: 'January 6, 1990',
+    },
+    nationality: {
+      title: 'Nationality',
+      content: 'Taiwan',
+    },
+    languages: {
+      title: 'Languages',
+      content: '中文, English',
+    },
+  },
+  contact: {
+    address1: {
+      title: 'Address',
+      content: '3F, No.378, Nanxiang Rd.,',
+    },
+    address2: {
+      title: '',
+      content: 'Luzhu District,',
+      noPrint: true,
+    },
+    address3: {
+      title: '',
+      content: 'Taoyuan City, Taiwan, 33854',
+      noPrint: true,
+    },
+    phone: {
+      title: 'Phone',
+      content: '+886 931 077 193',
+    },
+    email: {
+      title: 'Email',
+      content: 'tvpsh2020@gmail.com',
+      link: 'mailto:tvpsh2020@gmail.com',
+    },
+    portfolio: {
+      title: 'Portfolio',
+      content: 'jimmmmy.com/portfolio',
+      link: 'https://jimmmmy.com/portfolio',
+    },
+  },
+  links: {
+    github: {
+      title: 'GitHub',
+      content: 'github.com/tvpsh2021',
+      link: 'https://github.com/tvpsh2021/',
+      icon: 'fa fa-github fa-2x',
+    },
+    linkedIn: {
+      title: 'LinkedIn',
+      content: 'linkedin.com/in/tvpsh2020',
+      link: 'https://www.linkedin.com/in/tvpsh2020/',
+      icon: 'fa fa-linkedin fa-2x',
+    },
+    blog: {
+      title: 'Blog',
+      content: 'blog.jimmmmy.com',
+      link: 'https://blog.jimmmmy.com/',
+      icon: 'fa fa-wordpress fa-2x',
+    },
+    facebook: {
+      title: 'Facebook',
+      content: 'jimmmmy.cheng',
+      link: 'https://www.facebook.com/jimmmmy.cheng/',
+      icon: 'fa fa-facebook fa-2x',
+    },
+    instagram: {
+      title: 'Instagram',
+      content: '@tvpsh2020',
+      link: 'https://www.instagram.com/tvpsh2020/',
+      icon: 'fa fa-instagram fa-2x',
+    },
+  }
 };
