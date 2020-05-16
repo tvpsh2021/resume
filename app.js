c8b8815b3a3d55de97323f4cbadf187364d18857
Jimmy Cheng <tvpsh2021@gmail.com>
2020-05-16 18:42:08 +0800

    Several update
    - Resume content
    - Server secure configuration

--- app.js
+++ app.js
@@ -1,57 +1,56 @@
 const express = require('express');
 const compression = require('compression');
 const helmet = require('helmet');
-const app = express();
+const bunyan = require('bunyan');
+
 const SidebarData = require('./data/sidebar');
 const ResumeData = require('./data/resume');
-const isDevelopmentEnvironment = process.env.NODE_ENV === 'development' ? true : false;
 
-if (!isDevelopmentEnvironment) {
-  app.disable('x-powered-by');
-  app.enable('trust proxy');
-  app.use(helmet.hsts({
-    maxAge: 31536000,
-    includeSubDomains: false,
-    preload: false,
-    setIf: (req, res) => {
-      return req.secure || (req.headers['x-forwarded-proto'] === 'https');
-    }
-  }));
-  app.use((req, res, next) => {
+const app = express();
+const isDevEnvironment = process.env.NODE_ENV === 'development' ? true : false;
+const logger = bunyan.createLogger({
+  name: 'resume',
+  level: 'info'
+});
+
+app.use((req, res, next) => {
+  if (isDevEnvironment) {
     const regex = /www./gi;
     const processedURL = req.headers.host.replace(regex, '');
-
-    if (!req.secure) {
+    if (!req.secure || req.headers.host.includes('www.')) {
       res.redirect(301, 'https://' + processedURL + req.originalUrl);
+      return;
     }
+  }
+  next();
+});
 
-    if (req.headers.host.includes('www.')) {
-      res.redirect(301, 'https://' + processedURL + req.originalUrl);
-    }
+app.enable('trust proxy');
 
-    next();
-  });
-}
+app.use(helmet());
+app.use(compression());
+
+app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 8080);
-app.use(express.static(__dirname + '/public'));
-app.use(compression());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
   res.render('pages/resume.pug', { SidebarData, ResumeData });
 });
+
 app.get('/portfolio', (req, res) => {
   res.render('pages/portfolio.pug', { SidebarData });
 });
+
 app.use((req, res) => {
-  res.status(404);
-  res.json({
-    code: 404,
-    status: 'OK',
-    message: '404 Not Found.'
+  logger.warn({ path: req.url }, 'request from invalid path');
+  res.status(404).json({
+    message: 'Not found.'
   });
 });
+
 app.listen(app.get('port'), () => {
-  console.log('Node app is running on port ', app.get('port'));
+  logger.info({ port: app.get('port') }, 'resume server start');
 });
