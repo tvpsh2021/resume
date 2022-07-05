28081e9d07932902f5e45d83512fca8083b28962
Jimmy Cheng <tvpsh2020@gmail.com>
2022-07-05 15:55:19 +0800

    Several updates
    - CSP policy
    - Appengine runtime
    - Package upgrade

--- app.js
+++ app.js
@@ -20,9 +20,58 @@ app.use((req, res, next) => {
   next();
 });
 
-app.enable('trust proxy');
+app.set('trust proxy', true);
 
-// app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  useDefaults: false,
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: [
+      "'self'",
+      '*.fontawesome.com',
+      '*.googletagmanager.com',
+      '*.google-analytics.com',
+      "'unsafe-inline'",
+      '*.facebook.net'
+    ],
+    styleSrc: [
+      "'self'",
+      "'unsafe-inline'",
+      '*.googleapis.com'
+    ],
+    fontSrc: [
+      "'self'",
+      '*.fontawesome.com',
+      '*.gstatic.com',
+      'data:'
+    ],
+    connectSrc: [
+      "'self'",
+      '*.fontawesome.com',
+      '*.google-analytics.com',
+      '*.doubleclick.net'
+    ],
+    imgSrc: [
+      "'self'",
+      '*.google-analytics.com'
+    ],
+    upgradeInsecureRequests: []
+  }
+}));
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 app.use(compression());
 
 app.use(express.static(__dirname + '/public'));
@@ -39,11 +88,14 @@ app.get('/portfolio', (req, res) => {
   res.render('pages/portfolio.pug', { SidebarData });
 });
 
+// app.use('/robots.txt', function (req, res, next) {
+//   res.type('text/plain');
+//   res.send('User-agent: *\nDisallow: /');
+// });
+
 app.use((req, res) => {
   console.warn('request from invalid path:', req.url);
-  res.status(404).json({
-    message: 'Not found.'
-  });
+  res.redirect(301, '/');
 });
 
 app.listen(app.get('port'), () => {
